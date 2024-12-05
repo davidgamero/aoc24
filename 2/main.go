57106d698547eb490616477ea5185932747b076d
David Gamero <david340804@gmail.com>
2024-12-04 23:15:41 -0500

    day2 done

--- 2/main.go
+++ 2/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-  "os"
-  "bufio"
-  "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,48 +16,97 @@ func main() {
 	}
 	defer file.Close()
 
-  safeReports := [][]int{}
-  unsafeReports := [][]int{}
+	safeReports := [][]int{}
+	unsafeReports := [][]int{}
+	safeReportsDampened := [][]int{}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		report := strings.Split(line, " ")
-  }
+		reportStrings := strings.Split(line, " ")
+		newReport := []int{}
+		for _, s := range reportStrings {
+			i, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatal("unable to convert to integer: ", err)
+			}
+			newReport = append(newReport, i)
+		}
+		if reportIsSafe(newReport) {
+			safeReports = append(safeReports, newReport)
+		} else {
+			unsafeReports = append(unsafeReports, newReport)
+		}
+
+		if reportIsSafeDampener(newReport) {
+			safeReportsDampened = append(safeReportsDampened, newReport)
+		}
+	}
+
+	fmt.Printf("%d safe reports\n", len(safeReports))
+	fmt.Printf("%d safe reports (dampened)\n", len(safeReportsDampened))
 }
 
-func reportIsSafe([]int report) bool{
-  increased := false
-  decreased := false
+func reportIsSafe(report []int) bool {
+	increased := false
+	decreased := false
 
-  for i := 0; i < len(report) - 1; i++{
-    n1 = report[i]
-    n2 = report[i+1]
+	for i := 0; i < len(report)-1; i++ {
+		n1 := report[i]
+		n2 := report[i+1]
 
-    if n1 == n2{
-      return false
-    }
+		if n1 == n2 {
+			return false
+		}
 
-    // must be monotonically increasing or decreasing
-    if n2 > n1{
-      increased = true
-    }
-    if n2 < n1{
-      decreased = true
-    }
-    if decreased && increased{
-      return false
-    }
+		// must be monotonically increasing or decreasing
+		if n2 > n1 {
+			increased = true
+		}
+		if n2 < n1 {
+			decreased = true
+		}
+		if decreased && increased {
+			return false
+		}
 
-    // can only increase or decrease by 1,2,3
-    amount := n2 - n1
-    if amount < 0{
-      amount = -amount
-    }
-    if amount > 3 || amount < 1{
-      return false
-    }
-  }
+		// can only increase or decrease by 1,2,3
+		amount := n2 - n1
+		if amount < 0 {
+			amount = -amount
+		}
+		if amount > 3 || amount < 1 {
+			return false
+		}
+	}
 
-  return true
+	return true
+}
+
+func reportIsSafeDampener(report []int) bool {
+	fmt.Println("checking ", report)
+	if reportIsSafe(report) {
+		return true
+	}
+	fmt.Println("  not safe, checking dampened...")
+
+	// greedy time there's not that many
+	for i := range report {
+		reportwithouti := []int{}
+		// before i
+		if i > 0 {
+			reportwithouti = append(reportwithouti, report[0:i]...)
+		}
+		// after i
+		if i < len(report)-1 {
+			reportwithouti = append(reportwithouti, report[i+1:]...)
+		}
+		fmt.Println(" i=", i)
+		fmt.Println(" ", reportwithouti)
+		if reportIsSafe(reportwithouti) {
+			return true
+		}
+		fmt.Println(" not safe, continuing")
+	}
+	return false
 }
